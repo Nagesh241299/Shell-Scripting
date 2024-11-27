56f92440d1e2c5e9d02ea660cfb11f2847d4a08e
Harshit Sahu <101046816+harshitsahu2311@users.noreply.github.com>
2024-11-27 17:33:21 +0530

    Create for_loop.sh

---
+++ for_loop.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# This is for loops script which creates folder with passed arg 1 as name and arg2 as starting range to arg3 as ending range
+
+<< comment
+ 1 is argument 1 which is folder name
+ 2 is start range
+ 3 is end rangeask
+comment
+
+for (( num=$2 ; num<=$3; num++ ))
+do
+	mkdir "$1$num"
+done
+
+'
+Output: 
+./for_loop.sh Harshit 1 5
+ls
+Harshit1 Harshit2 Harshit3 Harshit4 Harshit5
+
+'
